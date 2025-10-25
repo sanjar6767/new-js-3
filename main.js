17893c903cab6c6aad61c1ccc2dd1148756db5d3
sanjar6767 <rasulberdievsanjar@gmail.com>
2025-10-25 19:12:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log(Math);
+
+let random = Math.ceil(Math.random()*10);
+
+console.log(random);
+
+alert("sizda hozir random son chiqadi😉")
+
+confirm("siz uchun chiqarilgan random son: " + random);
+
+confirm("siz uni 2 kopaytirishni  hohlaysizmi");
+
+alert("random son " + random + "\n sonni 2 ga kopaytirganda: " +  random*2 + "\n sonni 2 bolganda: " + random/2  );
+
